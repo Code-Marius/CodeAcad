b877e39ded8a802219d2fb27f1c2fdca665b8aa9
Code-Marius <marius.lekys@codeacademylt.onmicrosoft.com>
2025-01-05 15:01:07 -0500

    Added farewell and ask_question functions to test_script.py

--- test_script.py
+++ test_script.py
@@ -2,6 +2,13 @@
 def greet(name):
     return f"Hello, {name}!"
 
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+def ask_question(name):
+    return f"How are you doing, {name}?"
+
 if __name__ == "__main__":
     print(greet("DevSecOps"))
-
+    print(ask_question("DevSecOps"))
+    print(farewell("DevSecOps"))
